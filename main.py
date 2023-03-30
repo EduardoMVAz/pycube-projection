17d30e72f96ad9dc5240d9a84ff0e0145efdcff5
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2023-03-30 17:07:03 -0300

    cube first try

---
+++ main.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pygame
+
+
+class Cube:
+
+    WIDTH = 600
+    LENGTH = 600
+    FPS = 60
+
+    def __init__(self, d=-1):
+        pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.screen = pygame.display.get_surface()
+        self.clock = pygame.time.Clock()
+
+        self.d = d
+        self.projection_matrix = np.array([[1, 0, 0, 0],
+                                            [0, 1, 0, 0],
+                                            [0, 0, 0, -d],
+                                            [0, 0, -1/d, 0]])
+
+    def play(self):
+        '''
+        (200, 200, 200) (200, 400, 200) (400, 200, 200) (400, 400, 200)
+        (200, 200, 400) (200, 400, 400) (400, 200, 400) (400, 400, 400)
+        '''
+
+        # Linha 1 -> todos os X
+        # Linha 2 -> todos os y
+        # Linha 3 -> todos os z
+        # Linha 4 -> 1
+        cube = np.array([[200, 200, 400, 400, 200, 200, 400, 400], 
+                         [200, 400, 200, 400, 200, 400, 200, 400],
+                         [200, 200, 200, 200, 400, 400, 400, 400],
+                         [1, 1, 1, 1, 1, 1, 1, 1]])
+
+        rodando = True
+        while rodando:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    rodando = False
+            
+            self.clock.tick(self.FPS)
+
+            # Transformações
+
+            cube_projection = self.projection_matrix @ cube
+
+            # Draw
+            self.screen.fill((0, 0, 0))
+
+            # Vertices
+            for line in cube_projection.T:
+                pygame.draw.circle(self.screen, (255, 255, 255), (int(line[0]/line[3]), int(line[1]/line[3])), 5)
+            
+            # Lines
+
+
+        
+
+
+        quit()
+
+
+    def quit():
+        pygame.quit()
+    
+
+
+if __name__ == '__main__':
+    game = Cube()
+    game.play()
