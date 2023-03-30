559dbcb5a7771161c1b0c1ac40bb78655903a71f
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2023-03-30 18:01:09 -0300

    rotation working

--- main.py
+++ main.py
@@ -19,48 +19,112 @@ class Cube:
                                             [0, 0, 0, -d],
                                             [0, 0, -1/d, 0]])
 
+
+    def rotation_matrix(self, angle, type='x'):
+        import math
+        angle = math.radians(angle)
+        
+        if type == 'x':
+            R = np.array([[1, 0, 0, 0],
+                          [0, math.cos(angle), -math.sin(angle), 0],
+                          [0, math.sin(angle), math.cos(angle), 0],
+                          [0, 0, 0, 1]])
+        elif type == 'y':
+            R = np.array([[math.cos(angle), 0, math.sin(angle), 0],
+                          [0, 1, 0, 0],
+                          [-math.sin(angle), 0, math.cos(angle), 0],
+                          [0, 0, 0, 1]])
+        elif type == 'z':
+            R = np.array([[math.cos(angle), -math.sin(angle), 0, 0],
+                          [math.sin(angle), math.cos(angle), 0, 0],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]])
+
+        return R
+
+
     def play(self):
-        '''
-        (200, 200, 200) (200, 400, 200) (400, 200, 200) (400, 400, 200)
-        (200, 200, 400) (200, 400, 400) (400, 200, 400) (400, 400, 400)
-        '''
 
         # Linha 1 -> todos os X
         # Linha 2 -> todos os y
         # Linha 3 -> todos os z
         # Linha 4 -> 1
-        cube = np.array([[200, 200, 400, 400, 200, 200, 400, 400], 
-                         [200, 400, 200, 400, 200, 400, 200, 400],
-                         [200, 200, 200, 200, 400, 400, 400, 400],
+        cube = np.array([[1, 1, -1, -1, 1, 1, -1, -1], 
+                         [1, -1, -1, 1, 1, -1, -1, 1],
+                         [-1, -1, -1, -1, 1, 1, 1, 1],
                          [1, 1, 1, 1, 1, 1, 1, 1]])
 
+        angle_x = 0
+        angle_y = 0
+        angle_z = 0
         rodando = True
         while rodando:
+            angle_x += 1
+            angle_y += 1
+            angle_z += 1
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     rodando = False
             
             self.clock.tick(self.FPS)
 
-            # Transformações
 
-            cube_projection = self.projection_matrix @ cube
+            # Rotaciona o cubo
+            R_x = self.rotation_matrix(angle_x, 'x')
+            R_y = self.rotation_matrix(angle_y, 'y')
+            R_z = self.rotation_matrix(angle_z, 'z')
+
+            cube_ = R_z @ R_y @ R_x @ cube
+
+            cube_[2] = cube_[2] + 5
+
+            # Faz a projeção
+            cube_projection = self.projection_matrix @ cube_
+
+            # Normaliza (elimina o 4º elemento)
+            cube_projection = cube_projection / cube_projection[3]
+
+            # Multiplica as coordenadas por 400
+            cube_projection = cube_projection * 400
+
+            # Transporta a origem para o centro da tela
+            cube_projection[0] = cube_projection[0] + self.WIDTH/2
+            cube_projection[1] = cube_projection[1] + self.LENGTH/2
+
+            coordinates = cube_projection.T
 
             # Draw
             self.screen.fill((0, 0, 0))
 
             # Vertices
-            for line in cube_projection.T:
-                pygame.draw.circle(self.screen, (255, 255, 255), (int(line[0]/line[3]), int(line[1]/line[3])), 5)
+            for line in coordinates:
+                pygame.draw.circle(self.screen, (255, 255, 255), (int(line[0]), int(line[1])), 5)
             
-            # Lines
+            # The first 4 coordinates are the vertices of a square, connect them with lines
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[0][0]), int(coordinates[0][1])), (int(coordinates[1][0]), int(coordinates[1][1])), 3)
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[1][0]), int(coordinates[1][1])), (int(coordinates[2][0]), int(coordinates[2][1])), 3)
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[2][0]), int(coordinates[2][1])), (int(coordinates[3][0]), int(coordinates[3][1])), 3)
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[3][0]), int(coordinates[3][1])), (int(coordinates[0][0]), int(coordinates[0][1])), 3)
+
+            # The last 4 coordinates are the vertices of a square, connect them with lines
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[4][0]), int(coordinates[4][1])), (int(coordinates[5][0]), int(coordinates[5][1])), 3)
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[5][0]), int(coordinates[5][1])), (int(coordinates[6][0]), int(coordinates[6][1])), 3)
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[6][0]), int(coordinates[6][1])), (int(coordinates[7][0]), int(coordinates[7][1])), 3)
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[7][0]), int(coordinates[7][1])), (int(coordinates[4][0]), int(coordinates[4][1])), 3)
+
+            # Connect the vertices of the first square with the vertices of the second square
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[0][0]), int(coordinates[0][1])), (int(coordinates[4][0]), int(coordinates[4][1])), 3)
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[1][0]), int(coordinates[1][1])), (int(coordinates[5][0]), int(coordinates[5][1])), 3)
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[2][0]), int(coordinates[2][1])), (int(coordinates[6][0]), int(coordinates[6][1])), 3)
+            pygame.draw.line(self.screen, (255, 255, 255), (int(coordinates[3][0]), int(coordinates[3][1])), (int(coordinates[7][0]), int(coordinates[7][1])), 3)
+            
 
 
-        
+            pygame.display.update()
 
 
         quit()
-
 
     def quit():
         pygame.quit()
